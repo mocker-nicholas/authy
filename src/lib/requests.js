02736cdcd47fdf846ee4aa5d7912edfc672adb34
Nick Orth <nicholas.r.orth@gmail.com>
2022-06-13 18:46:03 -0500

    Removed gh pages build scripts, commented out dev url var for netlify deployment

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = "https://authnetserver.herokuapp.com/api";
-const devUrl = "http://localhost:8080";
+// const devUrl = "http://localhost:8080";
 
 export const request = axios.create({
   withCredentials: true,
