7ff3055ce700e4a9a388252fa0ce365c4e1e44c9
Nick Orth <nicholas.r.orth@gmail.com>
2022-06-22 07:51:47 -0500

    Edit prod url for production

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080/api";
+const baseUrl = "https://dolphin-app-rvmeg.ondigitalocean.app/api";
 // const devUrl = "http://localhost:8080/api";
 // prod url = https://dolphin-app-rvmeg.ondigitalocean.app/api
 
