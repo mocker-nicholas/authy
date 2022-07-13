29e9ccf47d0e626dbc32f9a125048baec4d7ef0f
Nick Orth <nicholas.r.orth@gmail.com>
2022-07-13 11:17:51 -0500

    1.	Add retry into cypress.config 2.	Add baseurl environment var to config – need to change page object functions and base to utilize env var

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportHeight: 1080,
-  viewportWidth: 1920,
+  viewportHeight: 880,
+  viewportWidth: 1720,
+  retries: {
+    runMode: 2,
+    openMode: 1,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -11,4 +15,8 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     excludeSpecPattern: [],
   },
+  // env: {
+  //   PROD_BASE: "https://main--benevolent-scone-9283d1.netlify.app/",
+  //   DEV_BASE: "http://localhost:3000/"
+  // },
 });
